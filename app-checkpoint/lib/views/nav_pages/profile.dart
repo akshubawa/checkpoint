0f8714c59011b0d07de7c440527ce920e232388b
akshubawa <akshubawa70@gmail.com>
2024-09-10 11:10:12 +0530

    working in pause play functionality

--- app-checkpoint/lib/views/nav_pages/profile.dart
+++ app-checkpoint/lib/views/nav_pages/profile.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:checkpoint/api/api_service.dart';
 import 'package:checkpoint/screen/ui/screen.dart';
 import 'package:checkpoint/views/auth/login_view.dart';
+import 'package:checkpoint/views/notification.dart';
 import 'package:checkpoint/widgets/circular_icons.dart';
 import 'package:checkpoint/widgets/side_menu_tile.dart';
 import 'package:checkpoint/widgets/snackbar/custom_snackbar.dart';
@@ -40,6 +41,10 @@ class _ProfileState extends State<Profile> {
   void logout(BuildContext context) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove('token');
+    // await prefs.remove('checkInStartTime');
+    // await prefs.setBool('inOffice', false);
+    // await prefs.setBool('isActive', false);
+
     CustomSnackbar.show(context, "Logged out successfully", "green");
 
     Navigator.pushAndRemoveUntil(
@@ -61,11 +66,11 @@ class _ProfileState extends State<Profile> {
   }
 
   String capitalizeWords(String input) {
-  return input.split(' ').map((word) {
-    if (word.isEmpty) return word;
-    return word[0].toUpperCase() + word.substring(1).toLowerCase();
-  }).join(' ');
-}
+    return input.split(' ').map((word) {
+      if (word.isEmpty) return word;
+      return word[0].toUpperCase() + word.substring(1).toLowerCase();
+    }).join(' ');
+  }
 
   Future<void> getProfileData() async {
     try {
@@ -85,14 +90,16 @@ class _ProfileState extends State<Profile> {
         setState(() {
           userDetails = jsonResponse['user'];
           userName = "${userDetails['firstName']} ${userDetails['lastName']}";
-          departmentName = capitalizeWords(userDetails['departmentName'] ?? 'Unknown');
-
+          departmentName =
+              capitalizeWords(userDetails['departmentName'] ?? 'Unknown');
         });
       } else if (jsonResponse["success"] == false) {
-        CustomSnackbar.show(context, "Failed to fetch User Profile. Please try again later.", "red");
+        CustomSnackbar.show(context,
+            "Failed to fetch User Profile. Please try again later.", "red");
       }
     } catch (e) {
-      CustomSnackbar.show(context, "Something went wrong. Please try again later.", "red");
+      CustomSnackbar.show(
+          context, "Something went wrong. Please try again later.", "red");
       print('Error in API call: $e');
     } finally {
       setState(() {
@@ -145,156 +152,165 @@ class _ProfileState extends State<Profile> {
             ),
           ),
           SafeArea(
-            child: 
-                Stack(
-                  children: [
-                    SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            const Text(
-                              'Profile',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                            ),
-                            const SizedBox(height: 10),
-                            const CircleAvatar(
-                              radius: 50,
-                              backgroundImage: NetworkImage(
-                                'https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724889600&semt=ais_hybrid',
+            child: Stack(
+              children: [
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Profile',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30),
+                        ),
+                        const SizedBox(height: 20),
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                            'https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724889600&semt=ais_hybrid',
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          userName, // Use the fetched user's name
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        Text(
+                          departmentName,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Expanded(
+                                child: CircularIcons(
+                                    icon: Icons.people_alt_rounded,
+                                    label: 'My Team',
+                                    color: Colors.blue),
                               ),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              userName, // Use the fetched user's name
-                              style: const TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87,
-                              ),
-                            ),
-                            Text(
-                              departmentName,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.grey,
-                              ),
-                            ),
-                            const SizedBox(height: 30),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Expanded(
-                                    child: CircularIcons(
-                                        icon: Icons.people_alt_rounded,
-                                        label: 'My Team',
-                                        color: Colors.blue),
-                                  ),
-                                  Expanded(
-                                    child: GestureDetector(
-                                      onTap: () {
-                                        Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                const Screen(initialPage: 1),
-                                          ),
-                                        );
-                                      },
-                                      child: const CircularIcons(
-                                          icon: Icons.calendar_month_rounded,
-                                          label: 'Attendence',
-                                          color: Colors.purple),
-                                    ),
-                                  ),
-                                  const Expanded(
-                                    child: CircularIcons(
-                                        icon: Icons.settings,
-                                        label: 'Settings',
-                                        color: Colors.orange),
-                                  ),
-                                  const Expanded(
-                                    child: CircularIcons(
-                                        icon: Icons.person,
-                                        label: 'My Profile',
-                                        color: Colors.blueAccent),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 40),
-                            Column(
-                              children: [
-                                SideMenuTile(
-                                  title: "Notifications",
-                                  icon: Icons.notifications_active,
-                                  onTap: () {},
-                                ),
-                                SideMenuTile(
-                                  title: "My Leaves",
-                                  icon: Icons.medical_services_rounded,
+                              Expanded(
+                                child: GestureDetector(
                                   onTap: () {
                                     Navigator.pushReplacement(
                                       context,
                                       MaterialPageRoute(
                                         builder: (context) =>
-                                            const Screen(initialPage: 2),
+                                            const Screen(initialPage: 1),
                                       ),
                                     );
                                   },
+                                  child: const CircularIcons(
+                                      icon: Icons.calendar_month_rounded,
+                                      label: 'Attendence',
+                                      color: Colors.purple),
                                 ),
-                                SideMenuTile(
-                                  title: "Customer Support",
-                                  icon: Icons.support_agent_rounded,
+                              ),
+                              const Expanded(
+                                child: CircularIcons(
+                                    icon: Icons.settings,
+                                    label: 'Settings',
+                                    color: Colors.orange),
+                              ),
+                              Expanded(
+                                child: GestureDetector(
                                   onTap: () {},
+                                  child: CircularIcons(
+                                      icon: Icons.person,
+                                      label: 'My Profile',
+                                      color: Colors.blueAccent),
                                 ),
-                                SideMenuTile(
-                                  title: "Logout",
-                                  icon: Icons.logout,
-                                  onTap: () {
-                                    logout(context);
-                                  },
-                                ),
-                                const Divider(
-                                  color: Colors.grey,
-                                  height: 1,
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      "Version: $_version",
-                                      style: TextStyle(
-                                          color: Colors.black.withOpacity(0.5),
-                                          fontSize: 14),
-                                    )
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 100,
-                                ),
-                              ],
-                            )
-                          ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      if (isLoading)
-                     
-                Container(
-            color: Colors.black.withOpacity(0.5), // Darken background
-            child: const Center(
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-              ),
-            ),
-          )
-                      
-                  ],
+                        const SizedBox(height: 40),
+                        Column(
+                          children: [
+                            SideMenuTile(
+                              title: "Notifications",
+                              icon: Icons.notifications_active,
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const NotificationPage()));
+                              },
+                            ),
+                            SideMenuTile(
+                              title: "My Leaves",
+                              icon: Icons.medical_services_rounded,
+                              onTap: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const Screen(initialPage: 2),
+                                  ),
+                                );
+                              },
+                            ),
+                            SideMenuTile(
+                              title: "Customer Support",
+                              icon: Icons.support_agent_rounded,
+                              onTap: () {},
+                            ),
+                            SideMenuTile(
+                              title: "Logout",
+                              icon: Icons.logout,
+                              onTap: () {
+                                logout(context);
+                              },
+                            ),
+                            const Divider(
+                              color: Colors.grey,
+                              height: 1,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Version: $_version",
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.5),
+                                      fontSize: 14),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 100,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
+                if (isLoading)
+                  Container(
+                    color: Colors.black.withOpacity(0.5), // Darken background
+                    child: const Center(
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  )
+              ],
+            ),
           ),
         ],
       ),
--- app-checkpoint/lib/widgets/circle_widgets/in_home_widget.dart
+++ app-checkpoint/lib/widgets/circle_widgets/in_home_widget.dart
@@ -8,58 +8,101 @@ class InHomeWidget extends StatelessWidget {
   const InHomeWidget({
     super.key,
     required this.screenWidth,
+    required this.inRange,
   });
 
   final double screenWidth;
+  final bool inRange;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: screenWidth,
-      height: screenWidth,
-      decoration: BoxDecoration(
-          color: const Color(0xff3F81DE).withOpacity(.15),
-          shape: BoxShape.circle),
+    return GestureDetector(
+      onTap: inRange
+          ? () {
+              // Add your onTap logic here
+            }
+          : null,
       child: Container(
-        margin: EdgeInsets.all(8),
+        width: screenWidth,
+        height: screenWidth,
         decoration: BoxDecoration(
+          color: inRange
+              ? const Color(0xff3F81DE).withOpacity(.15)
+              : Colors.grey.withOpacity(0.3),
           shape: BoxShape.circle,
-          gradient: const LinearGradient(
-            colors: [
-              Color(0xff3F81DE),
-              Color(0xFF9282DF),
-            ],
-            stops: [0.38, .75],
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: const Color(0xFF9282DF).withOpacity(.75),
-              blurRadius: 8,
-              offset: const Offset(-3, 7),
-            ),
-          ],
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                UIconsPro.regularRounded.tap,
-                color: Colors.white,
-                size: screenWidth * .25,
-              ),
-              Text(
-                "CHECK IN",
-                style: GoogleFonts.nunitoSans(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: inRange
+                      ? const LinearGradient(
+                          colors: [
+                            Color(0xff3F81DE),
+                            Color(0xFF9282DF),
+                          ],
+                          stops: [0.38, .75],
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                        )
+                      : null,
+                  boxShadow: inRange
+                      ? [
+                          BoxShadow(
+                            color: const Color(0xFF9282DF).withOpacity(.75),
+                            blurRadius: 8,
+                            offset: const Offset(-3, 7),
+                          ),
+                        ]
+                      : [],
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        UIconsPro.regularRounded.tap,
+                        color: inRange ? Colors.white : Colors.grey[600],
+                        size: screenWidth * .25,
+                      ),
+                      if (!inRange)
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            "Not in Range",
+                            style: GoogleFonts.nunitoSans(
+                              color: Colors.grey[600],
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      if (inRange)
+                        Text(
+                          "CHECK IN",
+                          style: GoogleFonts.nunitoSans(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+            // if (!inRange)
+            //   Center(
+            //     child: Container(
+            //       width: screenWidth,
+            //       height: screenWidth,
+            //       color: Colors.black.withOpacity(0.3),
+            //     ),
+            //   ),
+          ],
         ),
       ),
     );
